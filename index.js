13481b6c5d37eb011531d049fe230e90fbb27555
frank <franksavena@gmail.com>
2022-11-03 19:46:35 -0500

    commit tree ab6b7c35aa3161156b012c6dcce5cff93f4f2782

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+function submitData (){
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: 'steve@steve.com',
+        }),
+    
+    } 
+     return fetch('http://localhost:3000/users', configObject)
+        .then(res => res.json()) 
+        // .then(res => console.log(res))
+        .then ((res) => { 
+            let newDiv = document.createElement('div')
+             newDiv.textContent = res.id
+            document.querySelector('body').append(newDiv)
+            console.log(res)
+        })
+
+        .catch(error => {
+            alert("ERROR")
+            let errorDiv = document.createElement('div')
+             errorDiv.textContent = 'Unauthorized Access'
+            document.querySelector('body').append(errorDiv)
+            console.log(error)
+        })
+
+    
+        
+}
+submitData()
+
